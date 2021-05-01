955f082151528f545866fe379ee263135af9b77a
Mat_r <mrauch247@gmail.com>
2021-05-01 11:35:47 -0400

    Degrees

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # PtrWrangler
 
-Using Minimax, implement an AI to play Tic-Tac-Toe optimally.
-
-Getting Started
-Once in the directory for the project, run pip3 install -r requirements.txt to install the required Python package (pygame) for this project.
-Run python runner.py to play against your AI!
-
+# CS50AI Assignment 1
+# 0. Search (Degrees)
---
+++ degrees.py
@@ -0,0 +1,176 @@
+import csv
+import sys
+
+from util import Node, StackFrontier, QueueFrontier
+
+# Maps names to a set of corresponding person_ids
+names = {}
+
+# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+people = {}
+
+# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
+movies = {}
+
+
+def load_data(directory):
+    """
+    Load data from CSV files into memory.
+    """
+    # Load people
+    with open(f"{directory}/people.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            people[row["id"]] = {
+                "name": row["name"],
+                "birth": row["birth"],
+                "movies": set()
+            }
+            if row["name"].lower() not in names:
+                names[row["name"].lower()] = {row["id"]}
+            else:
+                names[row["name"].lower()].add(row["id"])
+
+    # Load movies
+    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            movies[row["id"]] = {
+                "title": row["title"],
+                "year": row["year"],
+                "stars": set()
+            }
+
+    # Load stars
+    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            try:
+                people[row["person_id"]]["movies"].add(row["movie_id"])
+                movies[row["movie_id"]]["stars"].add(row["person_id"])
+            except KeyError:
+                pass
+
+
+def main():
+    if len(sys.argv) > 2:
+        sys.exit("Usage: python degrees.py [directory]")
+    directory = sys.argv[1] if len(sys.argv) == 2 else "large"
+
+    # Load data from files into memory
+    print("Loading data...")
+    load_data(directory)
+    print("Data loaded.")
+
+    source = person_id_for_name(input("Name: "))
+    if source is None:
+        sys.exit("Person not found.")
+    target = person_id_for_name(input("Name: "))
+    if target is None:
+        sys.exit("Person not found.")
+
+    path = shortest_path(source, target)
+
+    if path is None:
+        print("Not connected.")
+    else:
+        degrees = len(path)
+        print(f"{degrees} degrees of separation.")
+        path = [(None, source)] + path
+        for i in range(degrees):
+            person1 = people[path[i][1]]["name"]
+            person2 = people[path[i + 1][1]]["name"]
+            movie = movies[path[i + 1][0]]["title"]
+            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+
+
+def shortest_path(source, target):
+    """
+    Returns the shortest list of (movie_id, person_id) pairs
+    that connect the source to the target.
+
+    If no possible path, returns None.
+    """
+
+    # Init first source actor state node, add to set of actors investigated
+    source_actor_state = Node(source, None, None)
+    ActorsAlreadyInvestigating = set()
+    ActorsAlreadyInvestigating.add(source)
+
+    # Source and target actor are the same...
+    if source == target:
+        print ("Source and target actor are the same...")
+        return []
+
+    frontier = QueueFrontier()
+    frontier.add(source_actor_state)
+
+    while not frontier.empty():   
+
+        star_node = frontier.remove()
+
+        # For each costar of this actor(star) in all his/her films
+        coStars = neighbors_for_person(star_node.state)
+        for movie_action, actor_state in coStars:
+            # If this actor is not already being investigated
+            if actor_state not in ActorsAlreadyInvestigating:
+                # Create a node for the coStar, to be added to the frontier (if not target)
+                coStar = Node(actor_state, star_node, movie_action)
+                if coStar.state == target:
+                    print ("Found link to target actor.")
+                    traceNode = coStar
+                    path = []
+                    while traceNode.parent is not None:
+                        path.append((traceNode.action, traceNode.state))
+                        traceNode = traceNode.parent
+                    path.reverse()
+                    return path
+
+                frontier.add(coStar)
+                ActorsAlreadyInvestigating.add(actor_state)
+
+    print ("Could not find a link between actors.")
+    return None
+
+
+def person_id_for_name(name):
+    """
+    Returns the IMDB id for a person's name,
+    resolving ambiguities as needed.
+    """
+    person_ids = list(names.get(name.lower(), set()))
+    if len(person_ids) == 0:
+        return None
+    elif len(person_ids) > 1:
+        print(f"Which '{name}'?")
+        for person_id in person_ids:
+            person = people[person_id]
+            name = person["name"]
+            birth = person["birth"]
+            print(f"ID: {person_id}, Name: {name}, Birth: {birth}")
+        try:
+            person_id = input("Intended Person ID: ")
+            if person_id in person_ids:
+                return person_id
+        except ValueError:
+            pass
+        return None
+    else:
+        return person_ids[0]
+
+
+def neighbors_for_person(person_id):
+    """
+    Returns (movie_id, person_id) pairs for people
+    who starred with a given person.
+    """
+    movie_ids = people[person_id]["movies"]
+    neighbors = set()
+    for movie_id in movie_ids:
+        for person_id in movies[movie_id]["stars"]:
+            neighbors.add((movie_id, person_id))
+    return neighbors
+
+
+if __name__ == "__main__":
+    main()
